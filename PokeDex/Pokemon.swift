78a582e50173ae24b13deb875afdbea8368afdca
eidar <eidar@work-MacBook-Pro.local>
2016-07-29 17:23:22 +0200

    create pokemon model

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by eidar on 29.07.2016.
+//  Copyright © 2016 radek. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int){
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
